00122baa9c1236e60b508544d07d426d9063e471
anupam <anupamyadav0224@gmail.com>
2023-03-04 03:40:16 +0530

    startup script updated.

---
+++ terraform/scripts/install_httpd.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Update yum repos and install apache web server
+yum -y update
+yum -y install httpd
+systemctl enable httpd
+systemctl start httpd
+
+# Query google's metadata server to get the VM details
+NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
+IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
+cat <<EOF > /var/www/html/index.html
+<h1>Compute Engine</h1>
+<pre>
+Name: $NAME
+IP ADDRESS: $IP
+</pre>
+EOF
